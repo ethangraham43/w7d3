21b5cd32e72f91db83579f1c719d3c2169578689
Ethan Graham <ethangraham43@gmail.com>
2022-10-26 13:35:29 -0400

    set up user model

---
+++ goal_app/spec/factories/users.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+FactoryBot.define do
+  factory :user do
+    username {Faker::Internet.unique.username}
+    password {"password"}
+  end
+end
